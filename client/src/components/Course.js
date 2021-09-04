e82c7cd34e40837d2c206f9593131c2425bcdc0d
Brian Jiang <brianjiang2200@gmail.com>
2021-09-04 14:42:55 -0400

    list summatives course page

--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { withRouter } from 'react-router';
 import axios from 'axios';
 
+import '../css/Course.css';
+
 class Course extends Component {
 
   state = {
@@ -32,42 +34,49 @@ class Course extends Component {
 
   render() {
     let course = this.state.course[0];
+    console.log(course);
     return (
       <div className="course-wrapper">
-          <div className="course-title">
-            {course.title}
-          </div>
-          <div className="course-description">
-            {course.description}
-          </div>
-          <div className="summative-headers">
-            <div>
-              Name
+          <div className="course-row">
+            <div className="course-title">
+              {course.title}
             </div>
-            <div>
-              Weight
-            </div>
-            <div>
-              Grade
+            <div className="projected-avg">
+              Projected Average: <b>86.7</b>
             </div>
           </div>
-          <div className="course-summatives">
-            {course.summatives.map(summ => {
-              return (
-                <div className="summative-wrapper">
-                  <div className="summative-title">
-                    {summ.title}
-                  </div>
-                  <div className="summative-weight">
-                    {summ.weight}
-                  </div>
-                  <div className="summative-grade">
-                    {summ.grade}
-                  </div>
-                </div>
-              );
-            })}
+          <div className="course-row">
+            <div className="course-description">
+              {course.description}
+            </div>
+            <div className="cumulative">
+              Cumulative: <b>50.4</b>
+            </div>
           </div>
+          <hr/>
+          <div>Summatives</div>
+          <table className="table table-dark summative-table">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Weight</th>
+                <th scope="col">Grade</th>
+              </tr>
+            </thead>
+            <tbody>
+            {
+                course.summatives.map((summ, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{summ.title}</td>
+                      <td>{parseFloat(summ.weight['$numberDecimal']).toFixed(1)}</td>
+                      <td>{parseFloat(summ.grade['$numberDecimal']).toFixed(1)}</td>
+                    </tr>
+                  );
+                })
+              }
+            </tbody>
+          </table>
       </div>
     );
   }
